ec4bf4a38651e5f0c6d085738b2b995bfddfeca6
Moisés M <moiseselquimico@gmail.com>
2023-12-14 23:03:41 +0100

    fin

--- app/src/main/java/com/example/cifradocesar/MainActivity.kt
+++ app/src/main/java/com/example/cifradocesar/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultado: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,19 +19,25 @@ class MainActivity : AppCompatActivity() {
         val cifrarButton = findViewById<Button>(R.id.cifrar)
         val palabra = findViewById<EditText>(R.id.palabra)
         val desplazamiento = findViewById<EditText>(R.id.desplazamiento)
-        val resultado = findViewById<TextView>(R.id.resultado)
+        resultado = findViewById<TextView>(R.id.resultado)
 
 
         cifrarButton.setOnClickListener {
             if (palabra != null && desplazamiento != null) {
-                val palabraCifrada = cifrarCesar(palabra.text.toString(), desplazamiento.text.toString().toInt())
-                resultado.text = palabraCifrada
+                cifrarCorutina(palabra.text.toString(), desplazamiento.text.toString().toInt())
             } else {
 
             }
         }
+    }
 
 
+    fun cifrarCorutina(palabra: String, desplazamiento: Int) {
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val palabraCifrada = cifrarCesar(palabra, desplazamiento)
+            resultado.text = palabraCifrada
+        }
 
     }
 
@@ -35,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         return palabra.map { caracter ->
             when {
                 caracter.isLetter() -> {
-                    val base = if (caracter.isLowerCase()) 'a'.toInt() else 'A'.code
+                    val base = if (caracter.isLowerCase()) 'a'.code else 'A'.code
                     (((caracter.code - base + desplazamiento) % 26 + 26) % 26 + base).toChar()
                 }
                 else -> caracter
